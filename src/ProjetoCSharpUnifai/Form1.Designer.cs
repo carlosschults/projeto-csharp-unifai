f3106eeac8c8f2845ed320cde8a293b00855ecc6
Carlos Schults <carlos.schults@gmail.com>
2018-09-13 00:39:37 -0300

    Main form initial implementation

--- src/ProjetoCSharpUnifai/Form1.Designer.cs
+++ src/ProjetoCSharpUnifai/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ProjetoCSharpUnifai
+﻿namespace carlosschults.ProjetoCsharpUnifai.Exterior.Apresentacao.WinForm
 {
     partial class Form1
     {
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblInicial = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Editar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Excluir = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.txtPesquisarContato = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Telefone,
+            this.Email,
+            this.Editar,
+            this.Excluir});
+            this.dataGridView1.Location = new System.Drawing.Point(39, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(550, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblInicial
+            // 
+            this.lblInicial.AutoSize = true;
+            this.lblInicial.Location = new System.Drawing.Point(36, 114);
+            this.lblInicial.Name = "lblInicial";
+            this.lblInicial.Size = new System.Drawing.Size(100, 13);
+            this.lblInicial.TabIndex = 2;
+            this.lblInicial.Text = "Pesquisar contatos:";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.Name = "Telefone";
+            this.Telefone.ReadOnly = true;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
+            // Editar
+            // 
+            this.Editar.HeaderText = "Editar";
+            this.Editar.Name = "Editar";
+            this.Editar.ReadOnly = true;
+            // 
+            // Excluir
+            // 
+            this.Excluir.HeaderText = "Excluir";
+            this.Excluir.Name = "Excluir";
+            this.Excluir.ReadOnly = true;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(39, 326);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 3;
+            this.btnNovo.Text = "&Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // txtPesquisarContato
+            // 
+            this.txtPesquisarContato.Location = new System.Drawing.Point(142, 111);
+            this.txtPesquisarContato.Name = "txtPesquisarContato";
+            this.txtPesquisarContato.Size = new System.Drawing.Size(447, 20);
+            this.txtPesquisarContato.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 415);
+            this.Controls.Add(this.txtPesquisarContato);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.lblInicial);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblInicial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewButtonColumn Editar;
+        private System.Windows.Forms.DataGridViewButtonColumn Excluir;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.TextBox txtPesquisarContato;
     }
 }
 
--- src/ProjetoCSharpUnifai/Program.cs
+++ src/ProjetoCSharpUnifai/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProjetoCSharpUnifai
+namespace carlosschults.ProjetoCsharpUnifai.Exterior.Apresentacao.WinForm
 {
     static class Program
     {
